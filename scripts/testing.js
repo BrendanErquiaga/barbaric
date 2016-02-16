664f4974cdd4f971f75c05ee3698f99502a64ebe
brendanerquiaga <brendanerquiaga@gmail.com>
2016-02-16 15:25:06 -0700

    Created General Mixins file

--- scripts/testing.js
+++ scripts/testing.js
@@ -1,19 +1,14 @@
 var bob = new Human('bob', 22, 'a builder', 100, {});
-var stick = new Item('stick', 'toy');
+var stick = new Equipment('stick', 'toy', 'hand');
 var sword = new Equipment('fSword', 'weapon', 'hand');
 //var fSword = new FancyEquipment('big sword', 'great weapon', 'hand');
 
 function runTests(){
-        console.log('You have a ' + stick);
-        console.log('It has some properties', stick);
+    console.log("Beginning tests");
 
-        console.log('You have a ' + sword);
-        console.log('It has some properties', sword);
-//    console.log("Beginning tests");
-//
-//
-//    console.log("I made these: " + stick + ', ' + sword);
-//    bob.equip(stick);
-//    bob.equip(sword);
-//    console.log(bob.equipment);
+
+    console.log("I made these: " + stick + ', ' + sword);
+    bob.equip(stick);
+    bob.equip(sword);
+    console.log(bob.equipment);
 }
