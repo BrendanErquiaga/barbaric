e711f5d56e0aa51a5f75e4a01c6144d59b203dd3
brendanerquiaga <brendanerquiaga@gmail.com>
2016-02-16 15:19:36 -0700

    Yeaaaaa Inheritence with Mixins

--- scripts/items.js
+++ scripts/items.js
@@ -9,25 +9,10 @@ function Item(name, type) {
                 canBeIdentifiedByName(itemState));
 }
 
-function BasicItem(name, type){
-    this.name = name;
-    this.type = type;
-}
-
-function FancyEquipment(name, type, slot){
-    BasicItem.apply(this, [name, type]);
-    this.slot = slot;
-}
-FancyEquipment.prototype = Object.create(BasicItem.prototype);
-FancyEquipment.prototype.constructor = FancyEquipment;
-
 function Equipment(name, type, slot) {
     var equipmentState = {
-                name: name,
-                type: type,
                 slot: slot
                 };
 
-    return Object.assign(equipmentState,
-                    canBeIdentifiedByName(equipmentState));
+    return Object.assign(equipmentState, new Item(name,type));
 }
--- scripts/testing.js
+++ scripts/testing.js
@@ -1,11 +1,14 @@
 var bob = new Human('bob', 22, 'a builder', 100, {});
-//var stick = new Equipment('stick', 'weapon', 'hand');
-//var sword = new Equipment('sword', 'weapon', 'hand');
-var fSword = new FancyEquipment('big sword', 'great weapon', 'hand');
+var stick = new Item('stick', 'toy');
+var sword = new Equipment('fSword', 'weapon', 'hand');
+//var fSword = new FancyEquipment('big sword', 'great weapon', 'hand');
 
 function runTests(){
-        console.log(Item.prototype);
-        console.log(FancyEquipment.prototype);
+        console.log('You have a ' + stick);
+        console.log('It has some properties', stick);
+
+        console.log('You have a ' + sword);
+        console.log('It has some properties', sword);
 //    console.log("Beginning tests");
 //
 //
