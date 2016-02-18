a8b1ff4fec4e4304342d5e3f66990cd44d326403
brendanerquiaga <brendanerquiaga@gmail.com>
2016-02-18 15:21:47 -0700

    I found a code formatter weee

--- scripts/items.js
+++ scripts/items.js
@@ -1,26 +1,27 @@
 "use strict";
 
 function Item(name, type) {
-    var itemState = {
-        name: name,
-        type: type
-        };
-    return Object.assign(itemState,
-                canBeIdentifiedByName(itemState));
+
+  var itemState = {
+    name: name,
+    type: type
+  };
+
+  return Object.assign(itemState, canBeIdentifiedByName(itemState));
 }
 
 function ItemWithStats(name, type, stats) {
-    var state = {};
-    return Object.assign(state,
-                hasStats(state,stats),
-                new Item(name,type));
+  var state = {};
+
+  return Object.assign(state, hasStats(state, stats), new Item(name, type));
+
 }
 
 function Equipment(name, type, slot, stats) {
-    var equipmentState = {
-                slot: slot
-                };
 
-    return Object.assign(equipmentState,
-            new ItemWithStats(name,type,stats));
+  var equipmentState = {
+    slot: slot
+  };
+
+  return Object.assign(equipmentState, new ItemWithStats(name, type, stats));
 }
